f6ee9d84557830e2971712dd8dc96f428fa7550f
ftnext <takuyafjp+develop@gmail.com>
2025-02-07 14:55:19 +0900

    [feat] Shortcut of logger with NullHandler

--- src/happy_python_logging/__init__.py
+++ src/happy_python_logging/__init__.py
@@ -1,3 +1,10 @@
 # SPDX-FileCopyrightText: 2025-present ftnext <takuyafjp+develop@gmail.com>
 #
 # SPDX-License-Identifier: MIT
+import logging
+
+
+def getLoggerForLibrary(name: str) -> logging.Logger:  # noqa: N802
+    logger_for_library = logging.getLogger(name)
+    logger_for_library.addHandler(logging.NullHandler())
+    return logger_for_library
