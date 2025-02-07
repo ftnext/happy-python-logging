46364350a6d95ad01f9c9db06ed147d585959708
ftnext <takuyafjp+develop@gmail.com>
2025-02-07 14:58:28 +0900

    [docs] Why create getLoggerForLibrary

--- src/happy_python_logging/__init__.py
+++ src/happy_python_logging/__init__.py
@@ -5,6 +5,15 @@ import logging
 
 
 def getLoggerForLibrary(name: str) -> logging.Logger:  # noqa: N802
+    """Return a logger added a NullHandler.
+
+    If you are developing a library, you should add a NullHandler only.
+    See https://docs.python.org/3/howto/logging.html#configuring-logging-for-a-library
+
+    Equivalent::
+
+        logging.getLogger(name).addHandler(logging.NullHandler())
+    """
     logger_for_library = logging.getLogger(name)
     logger_for_library.addHandler(logging.NullHandler())
     return logger_for_library
