4b20f649be8270c1466b5e3591a32ce8e9054c32
ftnext <takuyafjp+develop@gmail.com>
2025-12-03 08:58:09 +0900

    refactor: Tests as spec

--- tests/lint/test_core.py
+++ tests/lint/test_core.py
@@ -5,30 +5,31 @@ from happy_python_logging.lint.core import ConfigureRootLoggerChecker
 
 
 class TestConfigureRootLoggerChecker:
-    def test_HPL101(self):  # noqa: N802
-        code = """\
+    class TestHPL101:
+        def test_import(self):
+            code = """\
 import logging
 
 def awesome():
     logging.basicConfig(level=logging.DEBUG)
 """
-        checker = ConfigureRootLoggerChecker()
-        checker.visit(ast.parse(code))
+            checker = ConfigureRootLoggerChecker()
+            checker.visit(ast.parse(code))
 
-        assert len(checker.errors) == 1
-        assert checker.errors[0] == (4, 4, ANY)
-        assert checker.errors[0][2].startswith("HPL101")
+            assert len(checker.errors) == 1
+            assert checker.errors[0] == (4, 4, ANY)
+            assert checker.errors[0][2].startswith("HPL101")
 
-    def test_HPL101_import_as(self):  # noqa: N802
-        code = """\
+        def test_import_as(self):
+            code = """\
 import logging as log
 
 def awesome():
     log.basicConfig(level=logging.DEBUG)
 """
-        checker = ConfigureRootLoggerChecker()
-        checker.visit(ast.parse(code))
+            checker = ConfigureRootLoggerChecker()
+            checker.visit(ast.parse(code))
 
-        assert len(checker.errors) == 1
-        assert checker.errors[0] == (4, 4, ANY)
-        assert checker.errors[0][2].startswith("HPL101")
+            assert len(checker.errors) == 1
+            assert checker.errors[0] == (4, 4, ANY)
+            assert checker.errors[0][2].startswith("HPL101")
