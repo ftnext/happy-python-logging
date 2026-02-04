7b0ce5ed78715be929bc0dc32486a2c65bf48596
ftnext <takuyafjp+develop@gmail.com>
2026-02-04 22:24:33 +0900

    feat: getLoggerForLibrary(with_handler-...)

--- src/happy_python_logging/__init__.py
+++ src/happy_python_logging/__init__.py
@@ -4,16 +4,25 @@
 import logging
 
 
-def getLoggerForLibrary(name: str) -> logging.Logger:  # noqa: N802
+def getLoggerForLibrary(name: str, with_handler: bool = True) -> logging.Logger:  # noqa: N802, FBT001, FBT002
     """Return a logger added a NullHandler.
 
-    If you are developing a library, you should add a NullHandler only.
+    If you are developing a library, you should typically add a NullHandler only.
     See https://docs.python.org/3/howto/logging.html#configuring-logging-for-a-library
 
-    Equivalent::
+    Args:
+        name: The name for the logger.
+        with_handler: Whether to add a NullHandler to the logger. Defaults to True.
+
+    Equivalent when with_handler=True::
 
         logging.getLogger(name).addHandler(logging.NullHandler())
+
+    When with_handler=False, functionally equivalent to ``logging.getLogger(name)``.
+    If no handler is added by the library user, the last resort handler is used.
+    https://docs.python.org/3/howto/logging.html#what-happens-if-no-configuration-is-provided
     """
     logger_for_library = logging.getLogger(name)
-    logger_for_library.addHandler(logging.NullHandler())
+    if with_handler:
+        logger_for_library.addHandler(logging.NullHandler())
     return logger_for_library
--- tests/test_happy_python_logging.py
+++ tests/test_happy_python_logging.py
@@ -3,8 +3,23 @@ import logging
 from happy_python_logging import getLoggerForLibrary
 
 
-def test_getLoggerForLibrary():  # noqa: N802
-    sut = getLoggerForLibrary("mylib")
+class TestGetLoggerForLibrary:
+    def test_default_adds_null_handler(self):
+        """Test that by default (backward compatibility), NullHandler is added."""
+        sut = getLoggerForLibrary("mylib")
 
-    assert len(sut.handlers) == 1
-    assert isinstance(sut.handlers[0], logging.NullHandler)
+        assert len(sut.handlers) == 1
+        assert isinstance(sut.handlers[0], logging.NullHandler)
+
+    def test_with_handler_true_adds_null_handler(self):
+        """Test that with_handler=True explicitly adds NullHandler."""
+        sut = getLoggerForLibrary("mylib.explicit", with_handler=True)
+
+        assert len(sut.handlers) == 1
+        assert isinstance(sut.handlers[0], logging.NullHandler)
+
+    def test_with_handler_false_does_not_add_handler(self):
+        """Test that with_handler=False does not add any handler."""
+        sut = getLoggerForLibrary("mylib.no_handler", with_handler=False)
+
+        assert len(sut.handlers) == 0
